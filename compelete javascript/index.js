730d54a28080cbe614f36b51464a53fa1fa73a4a
Gaurav Dubey <gauravd638@gmail.com>
2020-09-18 17:49:56 +0530

    project2 added

---
+++ compelete javascript/index.js
@@ -0,0 +1,20 @@
+console.log("hello form js file")
+
+// javascript print
+    //console.log("this is me");
+   // alert("me");
+   /* document.write("hey gaurav");
+   //javascript console api
+   console.error("this is error");
+   console.warn("hey woring");
+   console.log("this is console",4+12);
+ */
+
+   //js variable
+/*    var n=12;
+   var n1=13;
+   console.log(n+n1); */
+
+   //dom manipulation
+   let elen = document.getElementById('click');
+   console.log(elen);
