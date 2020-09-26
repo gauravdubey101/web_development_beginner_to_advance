016bde3fbdeba0458012c86f50d560000cf9972c
Gaurav Dubey <gauravd638@gmail.com>
2020-09-26 12:46:50 +0530

    JavaScript-6

---
+++ tut38.js
@@ -0,0 +1,26 @@
+console.log("This is tut38 ");
+
+function access(name,ntext="gaurav is web developer"){
+    console.log(ntext +" " + name);
+    console.log(name + " is web developer")
+}
+
+function sum(a,b){
+let c = a+b;
+return c;
+// return is last line of function
+}
+
+let n1 = "Gaurav";
+let n2= "Dubey";
+let n3 = "dev";
+let n4 = "sam";
+let ntext = "keepesh";
+access(n1,ntext);
+access(n2,ntext);
+access(n3,ntext);
+access(n4,ntext);
+
+
+let value = sum(5,4);
+console.log(value);
