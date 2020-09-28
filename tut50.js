433561f8a6eea1d373286e887e9c054d39fbd75c
Gaurav Dubey <gauravd638@gmail.com>
2020-09-28 23:56:23 +0530

    Node-read write file-2

---
+++ tut50.js
@@ -0,0 +1,10 @@
+//Asynchronous or non-blocking
+//line by line execution not guaranted
+//callbacks will fire
+
+const fs = require("fs");
+let text = fs.readFile("readf.txt","utf-8", (err,data)=>{
+    console.log(data);
+});
+console.log("tHIS IS  message");
+console.log(text);
