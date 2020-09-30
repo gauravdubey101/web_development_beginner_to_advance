cd037c0ce6aeaa9b36ba2ed6693552f154ee3e58
Gaurav Dubey <gauravd638@gmail.com>
2020-09-30 23:52:42 +0530

    express first app-1

---
+++ Express/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express();
+const port = 80;
+app.get("/",(req,res)=>{
+    res.send("This is my first express app with harry")
+});
+
+app.listen(port,()=>{
+    console.log(`The application started successfully on port ${port}`)
+})
