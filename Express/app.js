730487ad3c95bb9a51803e69fe90202ef8f64c37
Gaurav Dubey <gauravd638@gmail.com>
2020-10-01 00:01:06 +0530

    express first app-1

--- Express/app.js
+++ Express/app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express();
 const port = 80;
 app.get("/",(req,res)=>{
-    res.send("This is my first express app with harry")
+    res.send("This is my first express app.")
 });
 
 app.listen(port,()=>{
