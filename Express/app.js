fb798eb7e077f337b3fbf3c68e08c6ee4efae33e
Gaurav Dubey <gauravd638@gmail.com>
2020-10-01 23:18:15 +0530

    express first app-pug install

--- Express/app.js
+++ Express/app.js
@@ -2,9 +2,26 @@ const express = require("express")
 
 const app = express();
 const port = 80;
+
+//serve static filenode
+app.use('/static',express.static('static'));
+//set the template engine as pug
+
+app.set('view engine', 'pug');
+
 app.get("/",(req,res)=>{
-    res.send("This is my first express app.")
+    res.status(200).send("This is my first express app.");
 });
+
+app.get("/about",(req,res)=>{
+    res.send("This is about page of  my first express app.");
+});
+
+//post method
+app.post("/about",(req,res)=>{
+    res.send("This is post request about page of  my first express app.");
+});
+
 
 app.listen(port,()=>{
     console.log(`The application started successfully on port ${port}`)
